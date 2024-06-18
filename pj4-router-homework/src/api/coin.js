76b6bebb8b6532c7f3a0835c45e6ab88f15378f5
JiYeon <kong.hh77@gmail.com>
2024-06-18 13:32:36 +0900

    pj4 - api 뿌려주기

--- pj4-router-homework/src/api/coin.js
+++ pj4-router-homework/src/api/coin.js
@@ -4,7 +4,13 @@ export const getCoinList = async () => {
     return result.slice(0, 100); // 개수가 많아서 100개만 가져오게 해주었음.
 }
 
-
+export const getCoinDetail = async (coinId) => {
+    let result = await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`);
+    result = await result.json();
+    return result;
+}
 
 // export 만 쓰는것은, 여러개의 api를 가져와야 할 수도 있기 때문에.
 // object 방식으로 여러개 가져올 수도 있음.
+// axios 로 하면 json 으로 안 바꿔줘도 됨
+// fetch는 json 형식으로 안 주기 때문에 우리가 바꿔줘야함
--- pj4-router-homework/src/pages/CoinDetail/index.js
+++ pj4-router-homework/src/pages/CoinDetail/index.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getCoinDetail } from "../../api/coin";
 
 function CoinDetail() {
     const {coinId} = useParams()
+    const [coinInfo, setCoinInfo] = useState();
+    useEffect(() => {
+        getCoinInfo()
+    },[])
+
+    // async 를 바로 쓸 수 없기 때문에 아래와 같이 만들어서 위에서 불러오기
+    const getCoinInfo = async () =>{
+        try {
+            const result = await getCoinDetail(coinId);
+            setCoinInfo(result);
+        } catch {
+            console.log('error');
+        }
+        
+    }
+
     return (
         <div>
-            <h1>CoinDetail{coinId}</h1>
-            {/* <p>{`djfhdjhfhj${coinId}`}</p> */}
+            <h1>CoinDetail : {coinId}</h1>
+            <p>{coinInfo?.description}</p>
             <div>
                 
             </div>
