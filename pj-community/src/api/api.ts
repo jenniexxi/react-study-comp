3c434e2dd14df106eb2ad975b0e1a1fccd469f84
JiYeon <kong.hh77@gmail.com>
2024-07-04 10:26:08 +0900

    chore: env, api 네이밍 수정

--- pj-community/src/api/api.ts
+++ pj-community/src/api/api.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // axios url 써주기
 const axiosInstance = axios.create({
-  baseURL: "http://192.168.13.54:3030",
-  // baseURL: import.meta.env.VITE_SERVER_ADDR,
+  // baseURL: "http://192.168.13.54:3030",
+  baseURL: import.meta.env.VITE_API_SERVER,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
